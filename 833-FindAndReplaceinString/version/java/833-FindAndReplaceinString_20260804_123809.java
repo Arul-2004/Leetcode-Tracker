65b1b0de37037d83613e6a00b895ce293f9e5e61
Arularasi S <arularasisivakumar20@gmail.com>
2026-08-04 12:38:00 +0530

    Create 833-FindAndReplaceinString

---
+++ 833-FindAndReplaceinString/version/java/833-FindAndReplaceinString_20260804_123809.java
@@ -0,0 +1,16 @@
+// Last updated: 8/4/2026, 12:38:09 PM
+1class Solution {
+2    public int minFlipsMonoIncr(String s) {
+3       int zf=0;
+4       int of=0;
+5       for(char ch:s.toCharArray()){
+6        if(ch=='1'){
+7         zf++;
+8        }
+9        else{
+10            of=Math.min(of+1,zf);
+11        }
+12       }
+13       return of;
+14    }
+15}
