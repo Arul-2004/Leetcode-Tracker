f5887e4bca08a92a766e8b0b7fb8560856816d95
Arularasi S <arularasisivakumar20@gmail.com>
2026-09-11 12:31:53 +0530

    Create 904-FruitIntoBaskets

---
+++ 904-FruitIntoBaskets/version/java/904-FruitIntoBaskets_20260911_123221.java
@@ -0,0 +1,15 @@
+// Last updated: 9/11/2026, 12:32:21 PM
+1class Solution {
+2    public int strStr(String haystack, String needle) {
+3       for(int i=0;i<=haystack.length()-needle.length();i++){
+4        int j=0;
+5        while(j<needle.length()&&haystack.charAt(i+j)==needle.charAt(j)){
+6            j++;
+7        }
+8        if(j==needle.length()){
+9            return i;
+10        }
+11       }
+12       return -1; 
+13    }
+14}
